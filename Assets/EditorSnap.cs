76e6410897438dedeb32979f27645a1dfb96a2ef
JX <jxie1995@gmail.com>
2020-04-25 16:10:19 -0700

    1. initial commit 2. creating cube with quads for individual surface properties 3. ExecuteInEditMode script for cube snapping

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1f, 20f)][SerializeField] float snapSize = 10f;
+
+    // Start is called before the first frame update
+
+    void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / snapSize) * snapSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / snapSize) * snapSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / snapSize) * snapSize;
+
+        //transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+        transform.position = snapPos;
+    }
+}

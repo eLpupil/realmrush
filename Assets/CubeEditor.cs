04566c8d096532800d29ddf257cc16732af86d40
JX <jxie1995@gmail.com>
2020-04-27 11:44:10 -0700

    1. Grid system (Quads, editor script, TextMesh labels, coordinates) 2. Waypoints 3. PathFinder (Breadth First Search set up) 4. Enemy movement (Coroutines, IEnumerator return type, yield returns)

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+    //[Range(1f, 20f)][SerializeField] float gridSize = 10f;
+    Waypoint waypoint;
+
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x * gridSize,
+            0,
+            waypoint.GetGridPos().y * gridSize  
+        );
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
+        textMesh.text = labelText;
+
+        gameObject.name = "Cube " + labelText;
+    }
+}
---
+++ Assets/PathFinder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+        ExploreNeighbors(startWaypoint.GetGridPos());
+    }
+
+
+    private void LoadBlocks()
+    {
+        Waypoint[] WaypointList = FindObjectsOfType<Waypoint>();
+
+        foreach (Waypoint waypoint in WaypointList)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(waypoint.GetGridPos()))
+            {
+                Debug.LogWarning("Duplicate block: " + gridPos);
+            }
+            else {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.cyan);
+        endWaypoint.SetTopColor(Color.magenta);
+    }
+
+    private void ExploreNeighbors(Vector2Int vector2Int)
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            try
+            {
+                Vector2Int explorationCoordinates = vector2Int + direction;
+                print("Exploring: " + explorationCoordinates);
+                grid[explorationCoordinates].SetTopColor(Color.yellow);
+            }
+            catch
+            {
+                //do nothing
+            }
+        }
+    }
+}

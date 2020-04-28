80b83f365a949556cd2f935b3954e4c26db75eda
JX <jxie1995@gmail.com>
2020-04-28 15:43:18 -0700

    1. ParticleEffect for tower shooting 2. Update environment (block placement and skybox) 3. Script for tower enemy tracking and shooting 4. Script for enemy collision handling

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] GameObject explosionFX;
+    [SerializeField] GameObject hitFX;
+
+    [SerializeField] float hitPoints = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+
+        if (hitPoints <= Mathf.Epsilon)
+        {
+            //play explosionFX 
+            Destroy(gameObject);
+            print("object destroyed");        }
+
+    }
+
+    private void ProcessHit()
+    {
+        if (hitPoints > 0f)
+        {
+            //play hit animation hitFX
+            hitPoints = hitPoints - 5f;
+            print("object hit");
+        }
+    }
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+
+    private void Start()
+    {
+        FireProjectile();
+    }
+
+    private void FireProjectile()
+    {
+        ParticleSystem.EmissionModule particleEmission = this.GetComponentInChildren<ParticleSystem>().emission;
+        particleEmission.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+
+
+}
+
+

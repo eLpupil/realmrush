30501d7cd0b71ea07320098fd518b9c69b611bfc
JX <jxie1995@gmail.com>
2020-04-29 21:23:28 -0700

    1. tower controller script + circular queue to limit number of towers

---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    [SerializeField] int towerLimit;
+    [SerializeField] Tower towerPrefab;
+    [SerializeField] Transform parent;
+
+    Queue<Tower> towers = new Queue<Tower>();
+
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        if (towers.Count == towerLimit)
+        {
+            DestroyAndDequeueTower();
+            AddAndQueueTower(baseWaypoint);
+        }
+        else
+        {
+            AddAndQueueTower(baseWaypoint);
+        }
+    }
+
+    private void DestroyAndDequeueTower()
+    {
+        Tower towerRemoved = towers.Dequeue();
+        Destroy(towerRemoved);
+    }
+
+    private void AddAndQueueTower(Waypoint baseWaypoint)
+    {
+        Tower newTower = Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity, parent);
+        towers.Enqueue(newTower);
+    }
+}

0daa16c9cf02b0aced74d6557fcd9f9025e099a4
JX <jxie1995@gmail.com>
2020-04-30 15:44:33 -0700

    1. text UI to show enemies on screen and base hp

--- Assets/BaseHealth.cs
+++ Assets/BaseHealth.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseHealth : MonoBehaviour
 {
     [SerializeField] int hitPoints = 20;
+    [SerializeField] int hpDecrease = 1;
+
+    [SerializeField] Text baseHPText;
 
 
     public void DecreaseHealth()
     {
         if (hitPoints > 0)
         {
-            hitPoints = hitPoints - 1;
+            hitPoints = hitPoints - hpDecrease;
         }
-        if (hitPoints < 1)
+        if (hitPoints < hpDecrease)
         {
             //todo maybe add base explosion fx
             //todo end game screen?
             print("game over");
         }
+    }
+
+    public void Update()
+    {
+        baseHPText.text = hitPoints.ToString();
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class EnemySpawner : MonoBehaviour
@@ -8,6 +9,8 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] Enemy enemyPrefab; //The type limits the type of object that can be selected for the field
     [SerializeField] Transform parent;
     [Range(0.1f, 5f)] [SerializeField] float secondsBetweenSpawns = 2f;
+
+    [SerializeField] Text countText;
 
 
     // Start is called before the first frame update
@@ -23,5 +26,11 @@ public class EnemySpawner : MonoBehaviour
             Enemy newUnit = Instantiate(enemyPrefab, transform.position, Quaternion.identity, parent);
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
+    }
+
+    private void Update()
+    {
+        int enemyCount = FindObjectsOfType<Enemy>().Length;
+        countText.text = enemyCount.ToString();
     }
 }
